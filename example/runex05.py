8e30fd1d54785940690e4f37a772cf3c7f80c1aa
Mark A. Caprio <mcaprio-nd-edu>
2017-07-07 16:00:56 -0400

    runex05: Add example of providing stdin to executable

---
+++ example/runex05.py
@@ -0,0 +1,42 @@
+""" runex05.py
+
+  Simple example of providing stdin input to an executable.
+
+  Language: Python 3
+
+  M. A. Caprio
+  Department of Physics
+  University of Notre Dame
+
+  6/28/17 (mac): Created.
+
+"""
+
+import mcscript
+
+mcscript.init()
+
+##################################################################
+# main body
+##################################################################
+
+mcscript.call(
+    ["cat"],
+    input_lines=[
+        "",
+        "Dear World,",
+        "",
+        "   Hello!",
+        "",
+        "Your faithful script,",
+        # note use of run parameters from mcscript.parameters.run
+        mcscript.parameters.run.name
+        ]
+    )
+
+
+################################################################
+# termination
+################################################################
+
+mcscript.termination()
