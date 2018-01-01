b057590bae60dcd7f1d366618876dd0fffe8f573
Patrick Fasano <patrick@patrickfasano.com>
2018-01-01 12:38:27 -0600

    packaging: Add setup.py

--- mcscript/__init__.py
+++ mcscript/__init__.py
@@ -1,15 +1,15 @@
-""" mcscript package -- scripting setup, utilities, and task control for cluster runs 
+""" mcscript package -- scripting setup, utilities, and task control for cluster runs
 
   Language: Python 3
 
   M. A. Caprio
   Department of Physics, University of Notre Dame
 
-  6/5/13 (mac): Derived from earlier job.py, script.py, task.py complex (2/13).
-  1/22/14 (mac): Python 3 update.
-  5/13/15 (mac): Insert "future" statements for attempted Python 2 legacy support.
-  6/13/16 (mac): Restructure submodules and generate __init__.py loader file.
-  11/22/16 (mac): Continue restructuring submodules. 
+  + 06/05/13 (mac): Derived from earlier job.py, script.py, task.py complex (2/13).
+  + 01/22/14 (mac): Python 3 update.
+  + 05/13/15 (mac): Insert "future" statements for attempted Python 2 legacy support.
+  + 06/13/16 (mac): Restructure submodules and generate __init__.py loader file.
+  + 11/22/16 (mac): Continue restructuring submodules.
 
 """
 
---
+++ setup.py
@@ -0,0 +1,14 @@
+import inspect
+import mcscript
+from setuptools import setup
+
+setup(
+    name="mcscript",
+    version="0.0.1",
+    author="Mark A. Caprio, University of Notre Dame",
+    description=("Scripting setup, utilities, and task control for cluster runs"),
+    license="MIT",
+    packages=['mcscript'],
+    long_description=inspect.getdoc(mcscript),
+    classifiers=[],
+)
