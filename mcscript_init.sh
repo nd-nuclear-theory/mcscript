1dc0e8af4d4157a393d0741a3b908447cd1d3b6f
Mark A. Caprio <mcaprio-nd-edu>
2020-06-26 23:12:17 -0400

    Clean up installation procedure and instructions
    * INSTALL.md: Overhaul instructions
    
    * tools/qsubm: Default to "python3" as invocation for Python
      interpreter for qsubm.py
    
    * qsubm.py: Make several environment variables optional
    
    * Move older scripts to deprecated directory

--- mcscript_init.sh
+++ mcscript_init.sh
@@ -1,17 +1,1 @@
-# mcscript_init.csh
-#
-# Initialization code to source from .cshrc.
-#
-# Mark A. Caprio
-# University of Notre Dame
-#
-# 1/27/16 (pjf): Created (ba)sh version.
-
-export PATH=${MCSCRIPT_DIR}/tools:${PATH}
-# export PYTHONPATH=${MCSCRIPT_DIR}:${PYTHONPATH}
-
-#alias cdr='cd ${MCSCRIPT_WORK_HOME}/${MCSCRIPT_RUN_PREFIX}\!*'
-
-cdr() {
-    cd ${MCSCRIPT_WORK_HOME}/${MCSCRIPT_RUN_PREFIX}$1
-}
+deprecated/mcscript_init.sh
