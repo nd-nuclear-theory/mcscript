4c23406c45d8e63d0b806791d446394f5f3e09f1
Patrick Fasano <patrick@patrickfasano.com>
2021-01-29 14:30:58 -0500

    setup: Bump version number to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mcscript",
-    version="0.1.1",
+    version="0.1.2",
     author="Mark A. Caprio, Patrick J. Fasano, University of Notre Dame",
     description=("Scripting setup, utilities, and task control for cluster runs"),
     license="MIT",
