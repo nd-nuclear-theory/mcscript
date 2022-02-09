31f94700d957b986d4f4d1dc00c789870e70b499
Patrick Fasano <patrick@patrickfasano.com>
2022-02-08 23:13:27 -0500

    setup: Use find_packages

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="mcscript",
@@ -6,6 +6,6 @@ setup(
     author="Mark A. Caprio, Patrick J. Fasano, University of Notre Dame",
     description=("Scripting setup, utilities, and task control for cluster runs"),
     license="MIT",
-    packages=['mcscript'],
+    packages=find_packages(include='mcscript*'),
     classifiers=[],
 )
