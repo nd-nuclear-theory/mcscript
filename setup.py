3d79aa0541443ca3d6b8db67176723d41ce4bcc1
Patrick Fasano <patrick@patrickfasano.com>
2023-07-13 17:46:13 -0500

    pep420: Convert mcscript to namespace package

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name="mcscript",
@@ -6,7 +6,7 @@ setup(
     author="Mark A. Caprio, Patrick J. Fasano, University of Notre Dame",
     description=("Scripting setup, utilities, and task control for cluster runs"),
     license="MIT",
-    packages=find_packages(include=['mcscript*']),
+    packages=find_namespace_packages(include=['mcscript*']),
     entry_points={
         "console_scripts": [
             "qsubm = mcscript.qsubm:main",
