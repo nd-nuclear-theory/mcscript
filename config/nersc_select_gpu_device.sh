9f22aad24d0ac2e05702c53ecd5c20fb814aeedc
Mark A. Caprio <mcaprio-nd-edu>
2023-09-27 21:52:57 -0400

    config/select_gpu_device: Update comment

--- config/nersc_select_gpu_device.sh
+++ config/nersc_select_gpu_device.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# select_cpu_device wrapper script
+# select_gpu_device wrapper script
 # based on https://docs.nersc.gov/jobs/affinity/#perlmutter
 export CUDA_VISIBLE_DEVICES=$SLURM_LOCALID
 exec $*
